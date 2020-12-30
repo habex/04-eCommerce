0f884586326c8f6ef8771a9db55495303f67232a
habex <habensf@gmail.com>
2020-12-29 17:07:08 -0800

    SpringBootTest error fixed

--- src/test/java/com/udacity/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/udacity/ecommerce/ECommerceApplicationTests.java
@@ -1,17 +1,15 @@
 package com.udacity.ecommerce;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest(classes = ECommerceApplication.class)
 public class ECommerceApplicationTests {
 
-	//@Test
-	public void contextLoads()  {
 
+	@Test
+	public void contextLoads()  {
 	}
 
 }
